9402ed3394f19a7ee99fb87ce9e17f51709b9ba3
MaraLfs <99500975+MaraLfs@users.noreply.github.com>
2022-07-21 18:02:34 +0200

    degrees

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Experience;
+use App\Repository\DegreeRepository;
 use App\Repository\ExperienceRepository;
 use App\Repository\TechnoRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,17 +13,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function home(TechnoRepository $technoRepository, ExperienceRepository $experienceRepository): Response
+    public function home(TechnoRepository $technoRepository, ExperienceRepository $experienceRepository, DegreeRepository $degreeRepository): Response
     {
         $frontTechnos = $technoRepository->findByFrontTechno();
         $backTechnos = $technoRepository->findByBackTechno();
 
         $experiences = $experienceRepository->findAll();
+        $degrees = $degreeRepository->findAll();
+
+
 
         return $this->render('base.html.twig', [
             'frontTechnos' => $frontTechnos,
             'backTechnos' => $backTechnos,
             'experiences' => $experiences,
+            'degrees' => $degrees,
         ]);
     }
 }
